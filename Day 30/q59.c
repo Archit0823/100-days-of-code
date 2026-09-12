9439d5f3b1dbdc2a4effc4d74040186e13ef315f
Archit <archit0823@gmail.com>
2026-09-12 10:00:01 +0530

    q59

---
+++ Day 30/q59.c
@@ -0,0 +1,37 @@
+//Q59: Count even and odd numbers in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+1 2 3 4 5 6
+Output 1:
+Even=3, Odd=3
+
+Input 2:
+4
+2 4 6 8
+Output 2:
+Even=4, Odd=0
+
+*/
+#include <stdio.h>
+int main () {
+    int n,i,e=0,o=0 ;
+    printf("Enter the length of array");
+    scanf("%d" ,&n);
+    int a[n];
+    for (i=0;i<n;i++) {
+        printf("Enter the element");
+        scanf("%d" ,&a[i]);
+    }
+    for (i=o;i<n;i++) {
+        if (a[i]%2==0) {
+            e++;
+        } else {
+            o++;
+        }
+    }
+    printf("Even =%d Odd =%d" ,e,o);
+    return 0;
+}
